3c09edcd9fb79c3d7d230d542f71700a707f5abb
Josom0 <jorge.solis@homie.mx>
2025-04-15 22:33:41 -0600

    button2

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -109,7 +109,7 @@ const Hero = () => {
 
           <motion.a
             href='#projects'
-            className='inline-block mt-8 px-6 py-3 bg-[#e9c0e9] text-[#292522] text-2xl font-extrabold rounded-xl mb-5 shadow transition-all hover:bg-[#d2e823] hover:text-[#1e2330] hover:shadow-lg'
+            className='inline-block mt-8 px-6 py-3 bg-[#e9c0e9] text-[#292522] text-2xl font-extrabold rounded-xl mb-7 shadow transition-all hover:bg-[#d2e823] hover:text-[#1e2330] hover:shadow-lg'
           >
             See My Work
           </motion.a>
